ec338a7d27c0ba4d7bad6cac76ab23be9baaa5f6
Maykeye <Maykeye@users.noreply.github.com>
2025-09-04 20:47:04 +0600

    Squashed commit of grab_cursor:
    commit 758e6349079802e5515972918daba42e074813ee
    Author: Maykeye <Maykeye@users.noreply.github.com>
    Date:   Thu Sep 4 20:46:12 2025 +0600
    
        Added grab_focused_window to grab cursor
    
        Now mouse cursor is grabbed by default.
        It can be changed by pressing Esc.

--- src/player_control_plugin.rs
+++ src/player_control_plugin.rs
@@ -63,9 +63,51 @@ fn player_cmds(mut evs: EventReader<PlayerCommand>, mut exit: EventWriter<AppExi
     }
 }
 
+fn ensure_grabbed_cursor(window: &mut Window) {
+    use bevy::window::CursorGrabMode;
+    if window.cursor_options.grab_mode != CursorGrabMode::Locked {
+        window.cursor_options.visible = false;
+        window.cursor_options.grab_mode = CursorGrabMode::Locked;
+        window.title = "Press ESC to release the cursor.".to_string();
+    }
+}
+fn ensure_released_cursor(window: &mut Window) {
+    use bevy::window::CursorGrabMode;
+    if window.cursor_options.grab_mode != CursorGrabMode::None {
+        window.cursor_options.visible = true;
+        window.cursor_options.grab_mode = CursorGrabMode::None;
+        window.title = "Press ESC to grab the cursor.".to_string();
+    }
+}
+
+fn grab_focused_window(
+    mut window: Single<&mut Window, With<PrimaryWindow>>,
+    inputs: Res<ButtonInput<KeyCode>>,
+    mut discard_grabbing: Local<bool>,
+) {
+    // TODO: should we prevent reacting to MouseMotion/ButtonInput<MouseButton>
+    // if grabbing is discarded?
+
+    // TODO:: we use raw input avoiding PlayerCommand. Should probably fix:
+    // For example if we'll ever add text input, pressing escape there will change focus
+    // instead of discarding input
+    if inputs.just_pressed(KeyCode::Escape) {
+        *discard_grabbing = !*discard_grabbing;
+    }
+
+    let should_grab = window.focused && !*discard_grabbing;
+
+    if should_grab {
+        ensure_grabbed_cursor(&mut window);
+    } else {
+        ensure_released_cursor(&mut window);
+    }
+}
+
 impl Plugin for PlayerControlPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<PlayerCommand>();
+        app.add_systems(PreUpdate, grab_focused_window);
         app.add_systems(Update, spawn_camera.run_if(in_state(GameState::Init)));
         app.add_systems(
             PlayerInputPreUpdate,
